9316892af6c127135e9acdaf0584dc620e0a1359
Kezziah Ayuno <80931169+kezzayuno@users.noreply.github.com>
2023-01-08 16:13:24 -0700

    Added request to get python script from GitHub

--- lab-1.py
+++ lab-1.py
@@ -8,3 +8,7 @@ os.system('cmd /c "pip show requests"')
 google = requests.get('https://www.google.com/')
 print(google)
 
+# curl download itself / prints itself 
+lab_1 = requests.get('https://raw.githubusercontent.com/kezzayuno/cmput404/master/lab-1.py?token=GHSAT0AAAAAAB444ROW5ADJLW4NAHIIHW42Y53JVAA')
+print(lab_1)
+
